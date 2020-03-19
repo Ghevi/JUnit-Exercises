c9a77bbc3a96ca5616a4473e649b47ebef446b46
Ghevi <ghevi96@gmail.com>
2020-03-19 23:52:28 +0100

    On branch master  Changes to be committed: 	new file:   .idea/vcs.xml 	modified:   .idea/workspace.xml 	modified:   src/test/java/io/ghevi/MathUtilsTest.java 	modified:   target/test-classes/io/ghevi/MathUtilsTest.class

--- src/test/java/io/ghevi/MathUtilsTest.java
+++ src/test/java/io/ghevi/MathUtilsTest.java
@@ -1,15 +1,27 @@
 package io.ghevi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
+// Every test that runs, there will be a new instance of this whole class
+// DO NOT use the @Order annotation, tests should be decoupled from one another
+// DO NOT declare instance variables to use in the methods, except ofc for the class instance,
+// because isn't tied of the methods order (which junit execute randomly)
+
 class MathUtilsTest {
+
+    MathUtils mathUtils;
+
+    @BeforeEach // It will run before each Test
+    void init(){
+        mathUtils = new MathUtils();
+    }
 
     @Test
     void testAdd(){
-        MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.add(1, 1);
 
@@ -25,16 +37,15 @@ class MathUtilsTest {
 
     @Test
     void testDivide(){
-        MathUtils mathUtils = new MathUtils();
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw");
 
     }
 
     @Test
     void testComputeCircleRadius(){
-        MathUtils mathUtils = new MathUtils();
         assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
     }
+
 
 
 
